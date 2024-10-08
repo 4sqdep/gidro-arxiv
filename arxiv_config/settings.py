b161aa012bd9531e7c8573d5ffb714234b7beec5
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-10-08 12:16:06 +0500

    register API

--- arxiv_config/settings.py
+++ arxiv_config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "corsheaders",
+    'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
     'account',
