1354b9cda62846a8d1fb35306e1c4a3667e2d402
Erkin <erkin@MacBook-Pro-Erkin.local>
2024-10-11 16:35:36 +0500

    niddleware

--- arxiv_config/settings.py
+++ arxiv_config/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middleware.AllowIframeMiddleware'
 ]
 
 ROOT_URLCONF = 'arxiv_config.urls'
